3765700d416a7640c9fbf3e5aad3cda59a07fa7d
Emmilie Estabillo <emsestabillo@gmail.com>
2017-01-06 16:52:20 +0000

    Connect Users to Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new, :create]
+  
   def index
     @places = Place.all
   end
@@ -9,8 +10,8 @@ class PlacesController < ApplicationController
   end
   
   def create
-    Place.create(place_params)
-    redirect_to root_path
+   current_user.places.create(place_params)
+   redirect_to root_path
   end
 
   private
