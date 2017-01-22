c5cf09ffbb042e29a1d79f18c356f116aed343cf
Emmilie Estabillo <emsestabillo@gmail.com>
2017-01-22 03:35:09 +0000

    User dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'places#index'
-
   resources :places do
     resources :comments, only: :create
   end
+  resources :users, only: :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
